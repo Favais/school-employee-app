c8776f8a56e3316efd661570cc12cfc4051bf8f9
Aisosa Oghagbon <oghagbonfav@gmail.com>
2025-08-09 15:49:08 +0100

    added settings for production env

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider, App as AntApp, message } from 'antd'
 import AuthContextProvider from './contexts/AuthContext.jsx'
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || REACT_APP_USE_MSW === 'true') {
   const { worker } = await import('./mocks/node.js')
   await worker.start()
 }
